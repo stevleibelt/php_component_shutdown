6fdc7eb374e6f3cb8d41e14d6ddb11737099fc7e
stevleibelt <stev@leibelt.de>
2013-06-30 22:07:29 +0200

    Added RuntimeShutdown

--- source/Net/Bazzline/Component/Shutdown/RuntimeShutdown.php
+++ source/Net/Bazzline/Component/Shutdown/RuntimeShutdown.php
@@ -6,8 +6,72 @@
 
 namespace Net\Bazzline\Component\Shutdown;
 
-
-class RuntimeShutdown
+/**
+ * Class RuntimeShutdown
+ *
+ * @package Net\Bazzline\Component\Shutdown
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-06-30
+ */
+class RuntimeShutdown implements ShutdownInterface
 {
+    /**
+     * @var boolean
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-06-30
+     */
+    private $isRequested;
 
+    /**
+     * @var string
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-06-30
+     */
+    private $name;
+
+    /**
+     * {$inheritDoc}
+     */
+    public function request()
+    {
+        $this->isRequested = true;
+
+        return $this;
+    }
+
+    /**
+     * {$inheritDoc}
+     */
+    public function isRequested()
+    {
+        return (!is_null($this->isRequested) && ($this->isRequested == true));
+    }
+
+    /**
+     * {$inheritDoc}
+     */
+    public function cancel()
+    {
+        $this->isRequested = false;
+
+        return $this;
+    }
+
+    /**
+     * {$inheritDoc}
+     */
+    public function getName()
+    {
+        return (is_null($this->name)) ? __CLASS__ : $this->name;
+    }
+
+    /**
+     * {$inheritDoc}
+     */
+    public function setName($name)
+    {
+        $this->name = (string) $name;
+
+        return $this;
+    }
 }
